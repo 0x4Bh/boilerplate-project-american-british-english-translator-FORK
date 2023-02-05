f01491a6ad0070052bdf13f39548d13259f5dec3
0x4Bh <kadir3407@gmail.com>
2023-02-05 22:00:30 +0300

    feat: all the tasks are complete except the tests

--- components/translator.js
+++ components/translator.js
@@ -4,7 +4,39 @@ const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
 class Translator {
-
+    americanToBritish(input) {
+        Object.keys(americanToBritishSpelling).forEach((us) => {
+            const pattern = new RegExp(us, "gi");
+            input = input.replaceAll(pattern, "<span class=\"highlight\">" + americanToBritishSpelling[us] + "</span>");
+        });
+        Object.keys(americanOnly).forEach((us) => {
+            const pattern = new RegExp(`(?<!\\w|-)${us}(?!\\w)`, "gi");
+            input = input.replaceAll(pattern, "<span class=\"highlight\">" + americanOnly[us] + "</span>");
+        });
+        Object.keys(americanToBritishTitles).forEach((us) => {
+            const pattern = new RegExp(`(?<!-)${us}(?![^ ])`, "gi");
+            input = input.replaceAll(pattern, "<span class=\"highlight\">" + this.capitalize(americanToBritishTitles[us.toLowerCase()]) + "</span>");
+        });
+        return input.replace(/(\d?\d):(\d\d)/g, "<span class=\"highlight\">" + "$1.$2" + "</span>") // for clock
+    }
+    britishToAmerican(input) {
+        Object.keys(americanToBritishSpelling).forEach((uk) => {
+            const pattern = new RegExp(americanToBritishSpelling[uk], "gi");
+            input = input.replaceAll(pattern, "<span class=\"highlight\">" + uk + "</span>");
+        });
+        Object.keys(britishOnly).forEach((uk) => {
+            const pattern = new RegExp(`(?<!\\w|-)${uk}(?!\\w)`, "gi");
+            input = input.replaceAll(pattern, "<span class=\"highlight\">" + britishOnly[uk] + "</span>");
+        });
+        Object.keys(americanToBritishTitles).forEach((uk) => {
+            const pattern = new RegExp(`(?<!-)${americanToBritishTitles[uk]}(?![^ ])`, "gi");
+            input = input.replaceAll(pattern, "<span class=\"highlight\">" + this.capitalize(uk) + "</span>");
+        });
+        return input.replace(/(\d?\d).(\d\d)/g, "<span class=\"highlight\">" + "$1:$2" + "</span>") // for clock
+    }
+    capitalize(input) {
+        return input ? input.charAt(0).toUpperCase() + input.slice(1) : undefined;
+    }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,24 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
-  const translator = new Translator();
 
+  const translator = new Translator();
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const { text, locale } = req.body;
+      let translation = "";
+      if (text === undefined || text === null || !locale) res.json({ error: 'Required field(s) missing' });
+      else if (text === "") return res.json({ error: 'No text to translate' });
+      else if (locale !== "american-to-british" && locale !== "british-to-american") res.json({ error: 'Invalid value for locale field' });
+      else {
+        if (locale === "american-to-british") {
+          translation = translator.americanToBritish(text);
+        }
+        else if (locale === "british-to-american") {
+          translation = translator.britishToAmerican(text);
+        }
+        translation = text === translation ? "Everything looks good to me!" : translation;
+        res.json({ text, translation });
+      }
     });
 };
